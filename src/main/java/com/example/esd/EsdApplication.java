7f37f94c586a6438d7098d39829dd6bbfcccb4e5
AakashPatel22 <22aakashpatel@gmail.com>
2024-11-03 00:00:59 +0530

    bug fixed

--- src/main/java/com/example/esd/EsdApplication.java
+++ src/main/java/com/example/esd/EsdApplication.java
@@ -2,8 +2,9 @@ package com.example.esd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EsdApplication {
 
     public static void main(String[] args) {
