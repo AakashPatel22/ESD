33d1de3cdb860db236af8fbf8686a3615f4dc0f8
AakashPatel22 <22aakashpatel@gmail.com>
2024-11-19 23:19:18 +0530

    added authHelper for jwt check

--- src/main/java/com/example/esd/controller/CustomerController.java
+++ src/main/java/com/example/esd/controller/CustomerController.java
@@ -4,6 +4,11 @@ import com.example.esd.dto.CustomerRequest;
 import com.example.esd.entity.Customer;
 
 import com.example.esd.service.CustomerService;
+import com.example.esd.helper.AuthHelper;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +21,7 @@ import org.springframework.http.ResponseEntity;
 public class CustomerController {
 
     private final CustomerService customerService;
-
+    private final AuthHelper authHelper;
 
     @PostMapping("/register")
     public ResponseEntity<Customer> createCustomer(@RequestBody @Valid CustomerRequest.CustomerCreateRequest customerCreateRequest) {
@@ -28,5 +33,28 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.loginCustomer(customerLoginRequest));
     }
 
+    @GetMapping("/fetch")
+    public ResponseEntity<?> fetchCustomer(HttpServletRequest request) {
+        String email = authHelper.extractAndCheckJWT(request);
+        if (email == null) {
+            return ResponseEntity.status(401).body("Unauthorized: Invalid or expired token");
+        }
+        Customer customer = customerService.fetchCustomer(email);
+        return ResponseEntity.ok(customer);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<?> updateCustomer(@RequestBody @Valid CustomerRequest.CustomerUpdateRequest updateRequest, HttpServletRequest request) {
+        String email = authHelper.extractAndCheckJWT(request);
+        if (email == null) {
+            return ResponseEntity.status(401).body("Unauthorized: Invalid or expired token"); // Custom error response
+        }
+
+        Customer updatedCustomer = customerService.updateCustomer(email, updateRequest);
+
+        if (updatedCustomer == null) {
+            return ResponseEntity.status(404).body("User not found!");
+        }
+        return ResponseEntity.ok(updatedCustomer);
+    }
 
 }
