1ca80d3c8af3759952da7f197cc9c40e0350a174
AakashPatel22 <22aakashpatel@gmail.com>
2024-11-20 14:39:23 +0530

    added product crud  operations

--- src/main/java/com/example/esd/service/ProductService.java
+++ src/main/java/com/example/esd/service/ProductService.java
@@ -6,6 +6,10 @@ import com.example.esd.mapper.ProductMapper;
 import com.example.esd.repo.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -16,5 +20,29 @@ public class ProductService {
         product = repo.save(product);
         return product;
     }
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+    public Product getProductById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+    public Product updateProduct(Long id, ProductRequest.ProductRequestBody request) {
+        Product product = getProductById(id);
+        if(product != null) {
+            product.setName(request.name());
+            product.setPrice(request.price());
+            product.setUpdatedOn(LocalDateTime.now());
+        }
+        return product!=null?repo.save(product):null;
+    }
+    public boolean deleteProduct(Long id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+    
 }
 
